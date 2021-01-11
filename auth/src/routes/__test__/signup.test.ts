010c9ec64f2d4408ee916de0b8bacb5d1d36a92a
Prakash Sharma <prakashsharma91@gmail.com>
2021-01-12 00:15:19 +0530

    jest testing microservice

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,80 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+});
+
+it("return a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@testcom",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("return a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@testcom",
+      password: "pad",
+    })
+    .expect(400);
+});
+
+it("return a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "prakas@gamil.com",
+      password: "",
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "",
+      password: "anbonce",
+    })
+    .expect(400);
+});
+
+it("disallow duplicate email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("sets a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
